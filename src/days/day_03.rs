23dd0c668799c8b8cb9ae374d4a31f3ea9842582
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2022-12-05 20:08:37 +0100

    feat(day_03): part 2, meh

--- src/days/day_03.rs
+++ src/days/day_03.rs
@@ -43,32 +43,96 @@ use super::load_file;
 ///
 /// Find the item type that appears in both compartments of each rucksack. What is the sum of the
 /// priorities of those item types?
+/// --- Part Two ---
+/// As you finish identifying the misplaced items, the Elves come to you with another issue.
+///
+/// For safety, the Elves are divided into groups of three. Every Elf carries a badge that
+/// identifies their group. For efficiency, within each group of three Elves, the badge is the only
+/// item type carried by all three Elves. That is, if a group's badge is item type B, then all three
+/// Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be
+/// carrying any other item type.
+///
+/// The problem is that someone forgot to put this year's updated authenticity sticker on the
+/// badges. All of the badges need to be pulled out of the rucksacks so the new authenticity
+/// stickers can be attached.
+///
+/// Additionally, nobody wrote down which item type corresponds to each group's badges. The only way
+/// to tell which item type is the right one is by finding the one item type that is common between
+/// all three Elves in each group.
+///
+/// Every set of three lines in your list corresponds to a single group, but each group can have a
+/// different badge item type. So, in the above example, the first group's rucksacks are the first
+/// three lines:
+///
+/// vJrwpWtwJgWrhcsFMMfFFhFp
+/// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+/// PmmdzqPrVvPwwTWBwg
+/// And the second group's rucksacks are the next three lines:
+///
+/// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+/// ttgJtRGJQctTZtZT
+/// CrZsJsPPZsGzwwsLwLmpwMDw
+/// In the first group, the only item type that appears in all three rucksacks is lowercase r; this
+/// must be their badges. In the second group, their badge item type must be Z.
+///
+/// Priorities for these items must still be found to organize the sticker attachment efforts: here,
+/// they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+///
+/// Find the item type that corresponds to the badges of each three-Elf group. What is the sum of
+/// the priorities of those item types?
 pub fn day_03() {
     let data = load_file(3);
 
     let data_as_lines = data.split("\n");
 
-    let mut priorities = 0u64;
+    let mut priorities_part_1 = 0u64;
+    let mut priorities_part_2 = 0u64;
+
+    let mut group_hash_set = std::collections::HashSet::new();
+
+    let mut group_idx = 0;
+
+    fn char_priority(c: char) -> u64 {
+        let mut priority = 0;
+        if c.is_ascii_uppercase() {
+            priority += 26;
+        }
+        priority += (c.to_lowercase().next().unwrap() as u64) - 96;
+        priority
+    }
 
     for line in data_as_lines {
         let trimmed_line = line.trim();
+        if group_idx == 0 {
+            group_hash_set.clear();
+            group_hash_set.extend(trimmed_line.chars());
+        } else {
+            let tmp_group_hash_set = std::collections::HashSet::from_iter(trimmed_line.chars());
+            let intersection = group_hash_set.intersection(&tmp_group_hash_set);
+            group_hash_set =
+                std::collections::HashSet::from_iter(intersection.into_iter().map(|&x| x));
+        }
+
+        if group_idx == 2 {
+            for &char in group_hash_set.iter() {
+                priorities_part_2 += char_priority(char);
+            }
+        }
+
+        group_idx = (group_idx + 1) % 3;
+
         let (first_half, second_half) = trimmed_line.split_at(trimmed_line.len() / 2);
 
         let mut hash_set_first = std::collections::HashSet::new();
         hash_set_first.extend(first_half.chars());
-
-        let mut hash_set_second = std::collections::HashSet::new();
-        hash_set_second.extend(second_half.chars());
+        let hash_set_second = std::collections::HashSet::from_iter(second_half.chars());
 
         let common_char = hash_set_first.intersection(&hash_set_second);
-        for char in common_char {
-            if char.is_ascii_uppercase() {
-                priorities += 26;
-            }
-            let lower_case_char_as_u64 = (char.to_lowercase().next().unwrap() as u64) - 96;
-            priorities += lower_case_char_as_u64;
+        for &char in common_char {
+            priorities_part_1 += char_priority(char);
         }
     }
 
-    println!("Part 1: {priorities}")
+    println!("Part 1: {priorities_part_1}");
+    println!("Part 2: {priorities_part_2}");
 }
