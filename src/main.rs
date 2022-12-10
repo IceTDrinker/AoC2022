7e3a711ac046b9de61d7f79d6cc2f89450859177
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2022-12-10 18:16:22 +0100

    feat(day_05): part 1, meh

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod days;
 
-const DAY_FUNCS: [fn() -> (); 4] = [
+const DAY_FUNCS: [fn() -> (); 5] = [
     days::day_01::day_01,
     days::day_02::day_02,
     days::day_03::day_03,
     days::day_04::day_04,
+    days::day_05::day_05,
 ];
 
 fn main() {
