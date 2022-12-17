fe1a0a2aedea6efdcceacccdf9ecf9c8afa88013
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2022-12-17 19:56:28 +0100

    feat(day_13): part 1, pretty happy with this one

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod days;
 
-const DAY_FUNCS: [fn() -> (); 12] = [
+const DAY_FUNCS: [fn() -> (); 13] = [
     days::day_01::day_01,
     days::day_02::day_02,
     days::day_03::day_03,
@@ -13,6 +13,7 @@ const DAY_FUNCS: [fn() -> (); 12] = [
     days::day_10::day_10,
     days::day_11::day_11,
     days::day_12::day_12,
+    days::day_13::day_13,
 ];
 
 fn main() {
