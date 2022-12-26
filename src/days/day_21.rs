409266a00e5e183094f4dfd07d38f6284c4de169
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2022-12-26 17:59:55 +0100

    feat(day_21): part 2, did not work properly using integers -_-
    - seemed easier to converge this way

--- src/days/day_21.rs
+++ src/days/day_21.rs
@@ -52,21 +52,50 @@ use super::load_file;
 ///
 /// However, your actual situation involves considerably more monkeys. What number will the monkey
 /// named root yell?
+///
+/// --- Part Two ---
+/// Due to some kind of monkey-elephant-human mistranslation, you seem to have misunderstood a few
+/// key details about the riddle.
+///
+/// First, you got the wrong job for the monkey named root; specifically, you got the wrong math
+/// operation. The correct operation for monkey root should be =, which means that it still listens
+/// for two numbers (from the same two monkeys as before), but now checks that the two numbers
+/// match.
+///
+/// Second, you got the wrong monkey for the job starting with humn:. It isn't a monkey - it's you.
+/// Actually, you got the job wrong, too: you need to figure out what number you need to yell so
+/// that root's equality check passes. (The number that appears after humn: in your input is now
+/// irrelevant.)
+///
+/// In the above example, the number you need to yell to pass root's equality test is 301. (This
+/// causes root to get the same number, 150, from both of its monkeys.)
+///
+/// What number do you yell to pass root's equality test?
 pub fn day_21() {
     let data = load_file(21);
 
-    #[derive(Debug, PartialEq, Eq, Hash)]
-    enum Monkey {
-        Number(String, i64),
-        Compute(String, (String, String), fn(i64, i64) -> i64),
+    #[derive(Debug, PartialEq, Clone)]
+    enum Op {
+        Add(String, String),
+        Sub(String, String),
+        Mul(String, String),
+        Div(String, String),
+        Val(f64),
+    }
+
+    #[derive(Debug, PartialEq, Clone)]
+    struct Monkey {
+        pub name: String,
+        pub op: Op,
     }
 
     impl Monkey {
         pub fn name(&self) -> String {
-            match self {
-                Monkey::Number(name, _) => name.clone(),
-                Monkey::Compute(name, _, _) => name.clone(),
-            }
+            self.name.clone()
+        }
+
+        pub fn op(&self) -> &Op {
+            &self.op
         }
     }
 
@@ -77,10 +106,13 @@ pub fn day_21() {
 
         fn from_str(s: &str) -> Result<Self, Self::Err> {
             let (monkey_name, num_or_op) = s.trim().split_once(':').unwrap();
-            let num: Result<i64, _> = num_or_op.trim().parse();
+            let num: Result<f64, _> = num_or_op.trim().parse();
 
             if let Ok(num) = num {
-                return Ok(Monkey::Number(monkey_name.to_string(), num));
+                return Ok(Monkey {
+                    name: monkey_name.to_string(),
+                    op: Op::Val(num),
+                });
             }
 
             let mut op_iter = num_or_op.trim().split(' ');
@@ -89,14 +121,17 @@ pub fn day_21() {
             let rhs = op_iter.next().unwrap().to_string();
 
             let op_fn = match op {
-                "+" => |x, y| x + y,
-                "-" => |x, y| x - y,
-                "*" => |x, y| x * y,
-                "/" => |x, y| x / y,
+                "+" => Op::Add(lhs, rhs),
+                "-" => Op::Sub(lhs, rhs),
+                "*" => Op::Mul(lhs, rhs),
+                "/" => Op::Div(lhs, rhs),
                 _ => unreachable!(),
             };
 
-            Ok(Monkey::Compute(monkey_name.to_string(), (lhs, rhs), op_fn))
+            Ok(Monkey {
+                name: monkey_name.to_string(),
+                op: op_fn,
+            })
         }
     }
 
@@ -111,24 +146,37 @@ pub fn day_21() {
             (name, monkey)
         })
         .collect();
-    let mut monkey_results: HashMap<String, i64> = HashMap::new();
+    let mut monkey_results: HashMap<String, f64> = HashMap::new();
 
-    fn solve_monkeys(
+    fn solve_monkeys_part_1(
         monkey_map: &HashMap<String, Monkey>,
-        monkey_results: &mut HashMap<String, i64>,
+        monkey_results: &mut HashMap<String, f64>,
         monkey_name: &String,
-    ) -> i64 {
+    ) -> f64 {
         if let Some(&monkey_result) = monkey_results.get(monkey_name) {
             monkey_result
         } else {
             let monkey = monkey_map.get(monkey_name).unwrap();
-            let res = match monkey {
-                Monkey::Number(_, number) => *number,
-                Monkey::Compute(_, (lhs, rhs), op_fn) => {
-                    let lhs_v = solve_monkeys(monkey_map, monkey_results, lhs);
-                    let rhs_v = solve_monkeys(monkey_map, monkey_results, rhs);
+            let res = if let Op::Val(val) = monkey.op() {
+                *val
+            } else {
+                let (lhs, rhs) = match monkey.op() {
+                    Op::Add(lhs, rhs)
+                    | Op::Sub(lhs, rhs)
+                    | Op::Mul(lhs, rhs)
+                    | Op::Div(lhs, rhs) => (lhs, rhs),
+                    _ => unreachable!(),
+                };
 
-                    op_fn(lhs_v, rhs_v)
+                let lhs_val = solve_monkeys_part_1(monkey_map, monkey_results, lhs);
+                let rhs_val = solve_monkeys_part_1(monkey_map, monkey_results, rhs);
+
+                match monkey.op() {
+                    Op::Add(_, _) => lhs_val + rhs_val,
+                    Op::Sub(_, _) => lhs_val - rhs_val,
+                    Op::Mul(_, _) => lhs_val * rhs_val,
+                    Op::Div(_, _) => lhs_val / rhs_val,
+                    _ => unreachable!(),
                 }
             };
 
@@ -137,7 +185,209 @@ pub fn day_21() {
         }
     }
 
-    let root_part_1 = solve_monkeys(&monkey_map, &mut monkey_results, &"root".to_string());
+    let root_part_1 = solve_monkeys_part_1(&monkey_map, &mut monkey_results, &"root".to_string());
 
     println!("Part 1: {root_part_1}");
+
+    let root_monkey = monkey_map.get(&"root".to_string()).unwrap();
+    let (lhs_root, rhs_root) = match root_monkey.op() {
+        Op::Add(lhs, rhs) | Op::Sub(lhs, rhs) | Op::Mul(lhs, rhs) | Op::Div(lhs, rhs) => {
+            (lhs.clone(), rhs.clone())
+        }
+        _ => unreachable!(),
+    };
+
+    // Like solve part 1 without memoization
+    fn compute_monkeys(monkey_map: &HashMap<String, Monkey>, monkey_name: &String) -> f64 {
+        let monkey = monkey_map.get(monkey_name).unwrap();
+        let res = if let Op::Val(val) = monkey.op() {
+            *val
+        } else {
+            let (lhs, rhs) = match monkey.op() {
+                Op::Add(lhs, rhs) | Op::Sub(lhs, rhs) | Op::Mul(lhs, rhs) | Op::Div(lhs, rhs) => {
+                    (lhs, rhs)
+                }
+                _ => unreachable!(),
+            };
+
+            let lhs_val = compute_monkeys(monkey_map, lhs);
+            let rhs_val = compute_monkeys(monkey_map, rhs);
+
+            match monkey.op() {
+                Op::Add(_, _) => lhs_val + rhs_val,
+                Op::Sub(_, _) => lhs_val - rhs_val,
+                Op::Mul(_, _) => lhs_val * rhs_val,
+                Op::Div(_, _) => lhs_val / rhs_val,
+                _ => unreachable!(),
+            }
+        };
+
+        res
+    }
+
+    let lhs = compute_monkeys(&monkey_map, &lhs_root);
+    let rhs = compute_monkeys(&monkey_map, &rhs_root);
+
+    let mut modified_monkey_map = monkey_map.clone();
+
+    let humn_name = "humn".to_string();
+
+    let mut lhs_constant = true;
+    let mut rhs_constant = true;
+
+    for idx in 0..i64::MAX {
+        modified_monkey_map.insert(
+            humn_name.clone(),
+            Monkey {
+                name: humn_name.clone(),
+                op: Op::Val(idx as f64),
+            },
+        );
+        let tmp_lhs = compute_monkeys(&modified_monkey_map, &lhs_root);
+        let tmp_rhs = compute_monkeys(&modified_monkey_map, &rhs_root);
+
+        if tmp_lhs != lhs {
+            println!("lhs: {tmp_lhs} != {lhs}");
+            lhs_constant = false;
+        }
+
+        if tmp_rhs != rhs {
+            println!("lhs: {tmp_rhs} != {rhs}");
+            rhs_constant = false;
+        }
+
+        if !lhs_constant || !rhs_constant {
+            break;
+        }
+    }
+
+    assert_ne!(lhs_constant, rhs_constant);
+
+    let (eval_branch_name, target, _pruned_branch_name) = if !lhs_constant {
+        (lhs_root, rhs, rhs_root)
+    } else {
+        (rhs_root, lhs, lhs_root)
+    };
+
+    let mut modified_monkey_map = monkey_map;
+
+    let mut min_bound = 0f64;
+    let mut max_bound = 1f64;
+
+    modified_monkey_map.insert(
+        humn_name.clone(),
+        Monkey {
+            name: humn_name.clone(),
+            op: Op::Val(min_bound),
+        },
+    );
+    let min_bound_eval = compute_monkeys(&modified_monkey_map, &eval_branch_name);
+
+    modified_monkey_map.insert(
+        humn_name.clone(),
+        Monkey {
+            name: humn_name.clone(),
+            op: Op::Val(max_bound),
+        },
+    );
+    let mut max_bound_eval = compute_monkeys(&modified_monkey_map, &eval_branch_name);
+
+    while min_bound_eval == max_bound_eval {
+        max_bound *= 2.0;
+
+        modified_monkey_map.insert(
+            humn_name.clone(),
+            Monkey {
+                name: humn_name.clone(),
+                op: Op::Val(max_bound),
+            },
+        );
+        max_bound_eval = compute_monkeys(&modified_monkey_map, &eval_branch_name);
+    }
+
+    let fn_increasing = min_bound_eval <= max_bound_eval;
+
+    println!("{fn_increasing}");
+
+    if target < max_bound_eval && !fn_increasing {
+        while target < max_bound_eval {
+            println!("{max_bound}");
+            max_bound *= 2.0;
+
+            modified_monkey_map.insert(
+                humn_name.clone(),
+                Monkey {
+                    name: humn_name.clone(),
+                    op: Op::Val(max_bound),
+                },
+            );
+            max_bound_eval = compute_monkeys(&modified_monkey_map, &eval_branch_name);
+        }
+    } else {
+        todo!("Code other cases yourself");
+    }
+
+    let mut res = 0.0;
+
+    while (min_bound - max_bound).abs() > 1.0 {
+        let mid = (min_bound + max_bound) / 2.0;
+
+        modified_monkey_map.insert(
+            humn_name.clone(),
+            Monkey {
+                name: humn_name.clone(),
+                op: Op::Val(mid),
+            },
+        );
+
+        let mid_eval = compute_monkeys(&modified_monkey_map, &eval_branch_name);
+
+        println!("target: {target}, mid_eval: {mid_eval}");
+        println!("min: {min_bound}, mid: {mid}, max: {max_bound}");
+
+        if mid_eval == target {
+            res = mid;
+            break;
+        }
+
+        if !fn_increasing {
+            if mid_eval >= target {
+                min_bound = mid;
+            } else {
+                max_bound = mid;
+            }
+        } else {
+            todo!("Code other cases yourself")
+        }
+    }
+
+    if res == 0.0 {
+        if min_bound_eval == target {
+            res = min_bound;
+        }
+        if max_bound_eval == target {
+            res = max_bound;
+        }
+    }
+
+    loop {
+        let new_res = res - 1.0;
+
+        modified_monkey_map.insert(
+            humn_name.clone(),
+            Monkey {
+                name: humn_name.clone(),
+                op: Op::Val(new_res),
+            },
+        );
+
+        let curr_eval = compute_monkeys(&modified_monkey_map, &eval_branch_name);
+        if curr_eval != target {
+            break;
+        } else {
+            res = new_res;
+        }
+    }
+
+    println!("Part 2: {res}");
 }
